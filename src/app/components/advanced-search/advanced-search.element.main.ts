a9bf805515fd1bb4bca56855de16a285c2aeced9
Abdella Ali <abdella@rangle.io>
2021-04-12 14:03:59 -0400

    final copy

---
+++ src/app/components/advanced-search/advanced-search.element.main.ts
@@ -0,0 +1,4 @@
+import { generateWCBootstrap } from '../../../angular-element-main.config';
+import { AdvancedSearchModule } from '@components/advanced-search/advanced-search.module';
+
+generateWCBootstrap(AdvancedSearchModule);
--- src/app/components/advanced-search/advanced-search.module.ts
+++ src/app/components/advanced-search/advanced-search.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AdvancedSearchComponent } from './advanced-search.component';
 import { FiltersModule } from '@components/filters';
 import { SearchBarModule } from '@components/search-bar';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  imports: [ReactiveFormsModule, FiltersModule, SearchBarModule],
+  imports: [ReactiveFormsModule, FiltersModule, SearchBarModule, BrowserModule],
   declarations: [AdvancedSearchComponent],
   exports: [AdvancedSearchComponent],
 })
-export class AdvancedSearchModule {}
+export class AdvancedSearchModule {
+  constructor(private injector: Injector){}
+  ngDoBootstrap(): void {
+    const el = createCustomElement(AdvancedSearchComponent, {injector : this.injector});
+    customElements.define('advanced-search-element', el);
+  }
+}
